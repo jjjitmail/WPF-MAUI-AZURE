5d929eb88756930d1d253d65993e12faafeb96fd
Tam <122633077+jjjitmail@users.noreply.github.com>
2025-02-12 11:04:12 +0100

    Add files via upload

---
+++ PSMAUI/PSTouchExpress/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿//using PSTouchExpress.Models;
+using PSTouchExpress.Pages;
+using Application = Microsoft.Maui.Controls.Application;
+
+namespace PSTouchExpress
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //if (Device.Idiom == TargetIdiom.Phone)
+            //    Shell.Current.CurrentItem = PhoneTabs;
+
+            //Routing.RegisterRoute(nameof(ConnectionsPage), typeof(ConnectionsPage));
+            //Routing.RegisterRoute(nameof(InstrumentPage), typeof(InstrumentPage));
+            //Routing.RegisterRoute(nameof(DataPage), typeof(DataPage));
+
+            //Routing.RegisterRoute("connections", typeof(ConnectionsPage));
+            //Routing.RegisterRoute("instrument", typeof(InstrumentPage));
+            //Routing.RegisterRoute("data", typeof(DataPage));
+
+            Page _page = new MainPage();
+            _page.BindingContext = new MainPageModel();
+            //var _page = new ShellPage();
+            //_page.BindingContext = new ConnectionsPageModel();
+
+            //var _ConnectionsPage = new ConnectionsPage();
+            //_ConnectionsPage.BindingContext = new ConnectionsPageModel();
+            //model.MainPageContentPresenter = _ConnectionsPage;
+
+            //_page.BindingContext = model;
+
+            MainPage = _page;
+
+            //var device = DeviceDisplay.MainDisplayInfo;
+
+            //MainPage = new MainPage();
+            //MainPage.BindingContext = new ConnectionsPageModel();
+
+            //var tabbedPage = new FreshMvvm.Maui.FreshTabbedNavigationContainer();
+            //tabbedPage.AddTab<MainPageModel>("Connections", null);
+            //tabbedPage.AddTab<MainPageModel>("Instrument", null);
+            //tabbedPage.AddTab<MainPageModel>("Data", null);
+
+            //MainPage = tabbedPage;
+        }
+    }
+}
---
+++ PSMAUI/PSTouchExpress/MauiProgram.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Maui.Controls.Compatibility;
+using Telerik.Maui.Controls.Compatibility;
+using PSTouchExpress.Pages;
+//using PSTouchExpress.Models;
+
+namespace PSTouchExpress
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+                .UseTelerik()
+                .UseMauiApp<App>()
+//#if ANDROID
+//                .ConfigureMauiHandlers(handlers =>
+//                {
+//                    //handlers.AddCompatibilityRenderer(typeof(Microsoft.Maui.Controls.BoxView),
+//                    //    typeof(Microsoft.Maui.Controls.Compatibility.Platform.Android.BoxRenderer));
+//                    //handlers.AddCompatibilityRenderer(typeof(Microsoft.Maui.Controls.Frame),
+//                    //    typeof(Microsoft.Maui.Controls.Compatibility.Platform.Android.FastRenderers.FrameRenderer));
+
+//                    // Register ALL handlers in the Xamarin Community Toolkit assembly
+//                    //handlers.AddCompatibilityRenderers(typeof(Xamarin.CommunityToolkit.UI.Views.MediaElementRenderer).Assembly)
+
+//                    // Register just one handler for the control you need
+//                    //handlers.AddCompatibilityRenderer(typeof(Xamarin.CommunityToolkit.UI.Views.MediaElement), typeof(Xamarin.CommunityToolkit.UI.Views.MediaElementRenderer));
+//                })
+//#elif IOS
+//                .ConfigureMauiHandlers(handlers =>
+//                {
+//                    //handlers.AddCompatibilityRenderer(typeof(Microsoft.Maui.Controls.BoxView),
+//                    //    typeof(Microsoft.Maui.Controls.Compatibility.Platform.iOS.BoxRenderer));
+//                    //handlers.AddCompatibilityRenderer(typeof(Microsoft.Maui.Controls.Frame),
+//                    //    typeof(Microsoft.Maui.Controls.Compatibility.Platform.iOS.FrameRenderer));
+
+//                    // Register ALL handlers in the Xamarin Community Toolkit assembly
+//                    //handlers.AddCompatibilityRenderers(typeof(Xamarin.CommunityToolkit.UI.Views.MediaElementRenderer).Assembly)
+
+//                    // Register just one handler for the control you need
+//                    //handlers.AddCompatibilityRenderer(typeof(Xamarin.CommunityToolkit.UI.Views.MediaElement), typeof(Xamarin.CommunityToolkit.UI.Views.MediaElementRenderer));
+//                })
+//#endif
+                .ConfigureFonts(fonts =>
+				{
+                    fonts.AddFont("Roboto-Regular.ttf", "DefaultTextFont");
+                    fonts.AddFont("segmdl2.ttf", "SegMDL2");
+				});
+
+            //builder.Services.AddSingleton<MainPage>(); //
+            //builder.Services.AddSingleton<ConnectionsPageModel>();
+
+            //builder.Services.AddTransient<MainPageModel>();
+            //builder.Services.AddTransient<MainPage>();
+
+            //builder.Services.UseFreshMvvm();
+
+            return builder.Build();
+		}
+	}
+}
