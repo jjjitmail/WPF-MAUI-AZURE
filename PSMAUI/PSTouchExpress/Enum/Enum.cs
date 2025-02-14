b196b60d0710ebedc4fd25fe6c4616bf6c56f9c4
Tam <122633077+jjjitmail@users.noreply.github.com>
2025-02-14 10:03:48 +0100

    Update Enum.cs

--- PSMAUI/PSTouchExpress/Enum/Enum.cs
+++ PSMAUI/PSTouchExpress/Enum/Enum.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PalmSens.Core.Presentation.MAUI
+namespace NN.Core.Presentation.MAUI
 {
     public enum ConnectionType
     {
